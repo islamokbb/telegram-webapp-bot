a1161622f352ff1e9713c01c3a7d3f176b53a56e
islamokbb <122336008+islamokbb@users.noreply.github.com>
2026-01-09 08:03:03 +0100

    Remove unused fetch import
    Removed unused 'fetch' import from index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const TelegramBot = require("node-telegram-bot-api");
-const fetch = require("node-fetch");
 
 // ================== ENV ==================
 const BOT_TOKEN = process.env.TELEGRAM_TOKEN;
